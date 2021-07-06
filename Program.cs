4d5bea641939d25bf9cd7b3117df72f38d3e70c1
M-jo2 <73107310+M-jo2@users.noreply.github.com>
2021-07-06 11:32:03 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoteurGraphiqueConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
